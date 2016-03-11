114630ed5c4a839a27e4497f232e5417d760a857
wujing <wuj@firstshare.cn>
2016-03-11 15:53:59 +0800

    Modify tree style

--- src/main/java/com/github/trace/web/CEPTraceController.java
+++ src/main/java/com/github/trace/web/CEPTraceController.java
@@ -26,12 +26,14 @@ public class CEPTraceController {
     public String callerList(@RequestParam(defaultValue = "最近1小时") String last, @RequestParam(defaultValue = "now() - 1h") String start, @RequestParam(defaultValue = "now()") String end, Model model) {
         List<Map> caller = influxRpcService.recentCaller(start, end);
         model.addAttribute("data", InnerUtil.toJsonService(caller));
-        model.addAttribute("start", start);
-        model.addAttribute("end", end);
-        model.addAttribute("last", TimePickerUtil.setLast(last, start, end));
-        model.addAttribute("times", Lists.newArrayList("10m", "30m", "1h", "6h", "12h", "1d", "2d", "3d", "5d"));
         return "cep/bp_list";
     }
+
+    @RequestMapping("/new")
+    public String createConfig(Model model) {
+        return "func/conf_create";
+    }
+
 
 
 }
