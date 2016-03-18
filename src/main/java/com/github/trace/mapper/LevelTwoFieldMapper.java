c280655b572bcce9bd20ac15dbe360b25d4016b1
chenlong <chenlong@fxiaoke.com>
2016-03-18 11:14:06 +0800

    添加"数据类型字段"映射类

---
+++ src/main/java/com/github/trace/mapper/LevelTwoFieldMapper.java
@@ -0,0 +1,8 @@
+package com.github.trace.mapper;
+
+import com.github.mybatis.mapper.ICrudMapper;
+import com.github.trace.entity.LevelTwoField;
+
+public interface LevelTwoFieldMapper extends ICrudMapper<LevelTwoField> {
+
+}
