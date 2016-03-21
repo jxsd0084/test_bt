439fdc51fefe17fa011764f33c1a55149f9199da
chenlong <chenlong@fxiaoke.com>
2016-03-21 09:58:18 +0800

    添加"导航树配置"入口

---
+++ src/main/java/com/github/trace/service/NavigationService.java
@@ -0,0 +1,14 @@
+package com.github.trace.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by chenlong on 2016/3/21.
+ */
+// 导航树
+@Service
+public class NavigationService {
+
+
+
+}
