0f3460287999ada820d8e14556e8f15aae3e7cf5
liyc <Jackie2015>
2016-03-22 11:27:49 +0800

    获取服务器端日志,取出第一条,并进行类型比较.

--- src/main/java/com/github/trace/intern/KafkaPropertyConstant.java
+++ src/main/java/com/github/trace/intern/KafkaPropertyConstant.java
@@ -14,7 +14,7 @@ public class KafkaPropertyConstant {
   public static final String FETCH_SIZE = "fetch.size";
 
   public static final int SERVER_PORT_DEFAULT = 9092;
-  public static final int CONSUMER_CONNECT_TIMEOUT_DEFAULT = 10000;
+  public static final int CONSUMER_CONNECT_TIMEOUT_DEFAULT = 60000;
   public static final int CONSUMER_BUFFER_SIZE_DEFAULT = 64 * 1024;
   public static final int FETCH_SIZE_DEFAULT = 10 * 1024;
 
