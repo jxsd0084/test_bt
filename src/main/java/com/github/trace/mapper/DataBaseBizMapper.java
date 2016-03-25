0b374d4399720d84865ae9f95a2975dc57fc7dcf
chenlong <chenlong@fxiaoke.com>
2016-03-25 18:01:39 +0800

    添加数据源配置数据库映射类

---
+++ src/main/java/com/github/trace/mapper/DataBaseBizMapper.java
@@ -0,0 +1,8 @@
+package com.github.trace.mapper;
+
+import com.github.mybatis.mapper.ICrudMapper;
+import com.github.trace.entity.DataBaseBiz;
+
+public interface DataBaseBizMapper extends ICrudMapper<DataBaseBiz>{
+
+}
---
+++ src/main/java/com/github/trace/mapper/DataBaseInfoMapper.java
@@ -0,0 +1,8 @@
+package com.github.trace.mapper;
+
+import com.github.mybatis.mapper.ICrudMapper;
+import com.github.trace.entity.DataBaseInfo;
+
+public interface DataBaseInfoMapper extends ICrudMapper<DataBaseInfo>{
+
+}
