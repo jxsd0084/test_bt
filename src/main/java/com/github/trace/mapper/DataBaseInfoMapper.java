36418ba4cc5d7fa7d860520f4855db435499c942
chenlong <chenlong@fxiaoke.com>
2016-03-28 15:22:02 +0800

    添加数据源的查询功能

--- src/main/java/com/github/trace/mapper/DataBaseInfoMapper.java
+++ src/main/java/com/github/trace/mapper/DataBaseInfoMapper.java
@@ -2,7 +2,14 @@ package com.github.trace.mapper;
 
 import com.github.mybatis.mapper.ICrudMapper;
 import com.github.trace.entity.DatabaseInfo;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 public interface DatabaseInfoMapper extends ICrudMapper<DatabaseInfo>{
+
+    @Select("SELECT * FROM database_info WHERE biz_id=#{id}")
+    List<DatabaseInfo> findDatabaseInfoListById(@Param("id") int id);
 
 }
