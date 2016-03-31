4093a02a451917b4064e67f89e73ebd473fced29
wzktravel <wzktravel@gmail.com>
2016-03-31 17:41:26 +0800

    add DatabaseBizMapper.java

---
+++ src/main/java/com/github/trace/mapper/DatabaseBizMapper.java
@@ -0,0 +1,8 @@
+package com.github.trace.mapper;
+
+import com.github.mybatis.mapper.ICrudMapper;
+import com.github.trace.entity.DatabaseBiz;
+
+public interface DatabaseBizMapper extends ICrudMapper<DatabaseBiz> {
+
+}
