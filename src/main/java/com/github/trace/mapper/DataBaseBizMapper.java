3bc6c24447de389363da21fb90bfc5e9e5ee3261
wzktravel <wzktravel@gmail.com>
2016-03-31 17:40:28 +0800

    delete DataBaseBizMapper.java

--- src/main/java/com/github/trace/mapper/DataBaseBizMapper.java
+++
@@ -1,8 +0,0 @@
-package com.github.trace.mapper;
-
-import com.github.mybatis.mapper.ICrudMapper;
-import com.github.trace.entity.DatabaseBiz;
-
-public interface DatabaseBizMapper extends ICrudMapper<DatabaseBiz>{
-
-}
