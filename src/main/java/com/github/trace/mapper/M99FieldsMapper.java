7049bb1564993c5229a2706a47123ee118980509
chenlong <chenlong@fxiaoke.com>
2016-04-05 14:10:01 +0800

    添加数据类型埋点M99扩展字段实体类

---
+++ src/main/java/com/github/trace/mapper/M99FieldsMapper.java
@@ -0,0 +1,8 @@
+package com.github.trace.mapper;
+
+import com.github.mybatis.mapper.ICrudMapper;
+import com.github.trace.entity.M99Fields;
+
+public interface M99FieldsMapper extends ICrudMapper<M99Fields> {
+
+}
