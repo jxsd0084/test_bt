0860658b63d0d4e955c789eeb4c15790d5f6f110
wanghl <wanghl@fxiaoke.com>
2016-04-06 20:50:31 +0800

    导航栏模块功能代码部分实现提交

---
+++ src/main/java/com/github/trace/mapper/NavigationItem0Mapper.java
@@ -0,0 +1,17 @@
+package com.github.trace.mapper;
+
+import com.github.mybatis.mapper.ICrudMapper;
+import com.github.trace.entity.NavigationItem0;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+/**
+ * Created by wanghl on 2016/3/31.
+ */
+public interface NavigationItem0Mapper extends ICrudMapper<NavigationItem0>{
+
+    @Select("SELECT * FROM navigation_item0 WHERE parent_id=#{parent_id}")
+    List<NavigationItem0> findByParentId(@Param("parent_id") int parent_id);
+}
