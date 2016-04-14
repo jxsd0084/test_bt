cf7e53c047f45f4f8766c267e33b4882fd86d06a
chenlong <chenlong@fxiaoke.com>
2016-04-14 16:57:58 +0800

    表单提交修改为POST

--- src/main/java/com/github/trace/web/DataTypeController.java
+++ src/main/java/com/github/trace/web/DataTypeController.java
@@ -5,16 +5,13 @@ import com.alibaba.fastjson.JSONObject;
 import com.github.trace.entity.LevelOneFields;
 import com.github.trace.entity.LevelTwoFields;
 import com.github.trace.entity.M99Fields;
-import com.github.trace.mapper.LevelOneFieldsMapper;
 import com.github.trace.service.CEPService;
 import com.github.trace.service.DataTypeService;
 import com.github.trace.utils.ControllerHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -106,50 +103,36 @@ public class DataTypeController {
         return "data/m99_edit";
     }
 
-    @RequestMapping("/modifyM99")
+    @RequestMapping(value = "/modifyM99", method = RequestMethod.POST)
     @ResponseBody
-    public Map modifyM99(@RequestParam("L1_id")   int    l1_id,
-                         @RequestParam("L1_tag")  String l1_tag,
-                         @RequestParam("L2_id")   int    l2_id,
-                         @RequestParam("L2_tag")  String l2_tag,
-                         @RequestParam("F1_name") String f1_name,
-                         @RequestParam("F1_desc") String f1_desc,
-                         @RequestParam("F1_type") String f1_type,
-                         @RequestParam("F1_regx") String f1_regx,
-                         @RequestParam("L3_id")   int l3_id) {
+    public Map modifyM99(@RequestBody JSONObject requestJson) {
         M99Fields m99Fields = new M99Fields();
-        m99Fields.setId(l3_id);
-        m99Fields.setLevelOneId(l1_id);
-        m99Fields.setM1Name(l1_tag);
-        m99Fields.setLevelTwoId(l2_id);
-        m99Fields.setFieldName(f1_name);
-        m99Fields.setFieldDesc(f1_desc);
-        m99Fields.setFieldType(f1_type);
-        m99Fields.setFieldRegex(f1_regx);
+        m99Fields.setId(requestJson.getInteger("L3_id"));
+        m99Fields.setLevelOneId(requestJson.getInteger("L1_id"));
+        m99Fields.setM1Name(requestJson.getString("L1_tag"));
+        m99Fields.setLevelTwoId(requestJson.getInteger("L2_id"));
+        m99Fields.setFieldName(requestJson.getString("F1_name"));
+        m99Fields.setFieldDesc(requestJson.getString("F1_desc"));
+        m99Fields.setFieldType(requestJson.getString("F1_type"));
+        m99Fields.setFieldRegex(requestJson.getString("F1_regx"));
 
         int res = dataTypeService.updateM99Fields(m99Fields);
 
         return ControllerHelper.returnResponseVal(res, "更新");
+
     }
 
-    @RequestMapping("/addM99")
+    @RequestMapping(value = "/addM99", method = RequestMethod.POST)
     @ResponseBody
-    public Map addM99(@RequestParam("L1_id")   int    l1_id,
-                      @RequestParam("L1_tag")  String l1_tag,
-                      @RequestParam("L2_id")   int    l2_id,
-                      @RequestParam("L2_tag")  String l2_tag,
-                      @RequestParam("F1_name") String f1_name,
-                      @RequestParam("F1_desc") String f1_desc,
-                      @RequestParam("F1_type") String f1_type,
-                      @RequestParam("F1_regx") String f1_regx) {
+    public Map addM99(@RequestBody JSONObject requestJson) {
         M99Fields m99Fields = new M99Fields();
-        m99Fields.setLevelOneId(l1_id);
-        m99Fields.setM1Name(l1_tag);
-        m99Fields.setLevelTwoId(l2_id);
-        m99Fields.setFieldName(f1_name);
-        m99Fields.setFieldDesc(f1_desc);
-        m99Fields.setFieldType(f1_type);
-        m99Fields.setFieldRegex(f1_regx);
+        m99Fields.setLevelOneId(requestJson.getInteger("L1_id"));
+        m99Fields.setM1Name(requestJson.getString("L1_tag"));
+        m99Fields.setLevelTwoId(requestJson.getInteger("L2_id"));
+        m99Fields.setFieldName(requestJson.getString("F1_name"));
+        m99Fields.setFieldDesc(requestJson.getString("F1_desc"));
+        m99Fields.setFieldType(requestJson.getString("F1_type"));
+        m99Fields.setFieldRegex(requestJson.getString("F1_regx"));
 
         int res = dataTypeService.addM99Fields(m99Fields);
 
@@ -283,39 +266,30 @@ public class DataTypeController {
         }
     }
 
-    @RequestMapping("/modifyLevelOne")
+    @RequestMapping(value = "/modifyLevelOne", method = RequestMethod.POST)
     @ResponseBody
-    public Map modifyLevelOne(@RequestParam("L1_tag")  String l1_tag,
-                              @RequestParam("L1_name") String l1_name,
-                              @RequestParam("navigationId") int navigation_id,
-                              @RequestParam("L1_desc") String l1_desc,
-                              @RequestParam("id") int id) {
+    public Map modifyLevelOne(@RequestBody JSONObject requestJson) {
         LevelOneFields levelOneFields = new LevelOneFields();
-        levelOneFields.setId(id);
-        levelOneFields.setLevel1FieldTag(l1_tag);
-        levelOneFields.setLevel1FieldName(l1_name);
-        levelOneFields.setLevel1FieldDesc(l1_desc);
-        levelOneFields.setNavigationId(navigation_id);
+        levelOneFields.setId(requestJson.getInteger("id"));
+        levelOneFields.setLevel1FieldTag(requestJson.getString("L1_tag"));
+        levelOneFields.setLevel1FieldName(requestJson.getString("L1_name"));
+        levelOneFields.setLevel1FieldDesc(requestJson.getString("L1_desc"));
+        levelOneFields.setNavigationId(requestJson.getInteger("navigationId"));
 
         int res = dataTypeService.updateFieldsByCascade(levelOneFields);
 
         return ControllerHelper.returnResponseVal(res, "更新");
-
     }
 
-    @RequestMapping("/modifyLevelTwo")
+    @RequestMapping(value = "/modifyLevelTwo", method = RequestMethod.POST)
     @ResponseBody
-    public Map modifyLevelTwo(@RequestParam("L1_tag")  String l1_tag,
-                              @RequestParam("L1_name") String l1_name,
-                              @RequestParam("L2_name") String l2_name,
-                              @RequestParam("L2_desc") String l2_desc,
-                              @RequestParam("L2_id") int l2_id) {
+    public Map modifyLevelTwo(@RequestBody JSONObject requestJson) {
         LevelTwoFields levelTwoFields = new LevelTwoFields();
-        levelTwoFields.setId(l2_id);
-        levelTwoFields.setLevel1FieldTag(l1_tag);
-        levelTwoFields.setLevel1FieldName(l1_name);
-        levelTwoFields.setLevel2FieldName(l2_name);
-        levelTwoFields.setLevel2FieldDesc(l2_desc);
+        levelTwoFields.setId(requestJson.getInteger("L2_id"));
+        levelTwoFields.setLevel1FieldTag(requestJson.getString("L1_tag"));
+        levelTwoFields.setLevel1FieldName(requestJson.getString("L1_name"));
+        levelTwoFields.setLevel2FieldName(requestJson.getString("L2_name"));
+        levelTwoFields.setLevel2FieldDesc(requestJson.getString("L2_desc"));
 
         int res = dataTypeService.updateLevelTwo(levelTwoFields);
 
@@ -323,17 +297,14 @@ public class DataTypeController {
 
     }
 
-    @RequestMapping("/addLevelOne")
+    @RequestMapping(value = "/addLevelOne", method = RequestMethod.POST)
     @ResponseBody
-    public Map addLevelOne(@RequestParam("L1_tag")  String l1_tag,
-                           @RequestParam("navigationId") int navigation_id,
-                           @RequestParam("L1_name") String l1_name,
-                           @RequestParam("L1_desc") String l1_desc) {
+    public Map addLevelOne(@RequestBody JSONObject requestJson) {
         LevelOneFields levelOneFields = new LevelOneFields();
-        levelOneFields.setLevel1FieldTag(l1_tag);
-        levelOneFields.setLevel1FieldName(l1_name);
-        levelOneFields.setNavigationId(navigation_id);
-        levelOneFields.setLevel1FieldDesc(l1_desc);
+        levelOneFields.setLevel1FieldTag(requestJson.getString("L1_tag"));
+        levelOneFields.setLevel1FieldName(requestJson.getString("L1_name"));
+        levelOneFields.setNavigationId(requestJson.getInteger("navigationId"));
+        levelOneFields.setLevel1FieldDesc(requestJson.getString("L1_desc"));
 
         int res = dataTypeService.addLevelOneFields(levelOneFields);
 
@@ -341,19 +312,16 @@ public class DataTypeController {
 
     }
 
-    @RequestMapping("/addLevelTwo")
+    @RequestMapping(value = "/addLevelTwo", method = RequestMethod.POST)
     @ResponseBody
-    public Map addLevelTwo(@RequestParam("L1_id")   int    l1_id,
-                           @RequestParam("L1_tag")  String l1_tag,
-                           @RequestParam("L1_name") String l1_name,
-                           @RequestParam("L2_name") String l2_name,
-                           @RequestParam("L2_desc") String l2_desc) {
+    public Map addLevelTwo(@RequestBody JSONObject requestJson) {
+
         LevelTwoFields levelTwoFields = new LevelTwoFields();
-        levelTwoFields.setLevel1FieldId(l1_id);
-        levelTwoFields.setLevel1FieldTag(l1_tag);
-        levelTwoFields.setLevel1FieldName(l1_name);
-        levelTwoFields.setLevel2FieldName(l2_name);
-        levelTwoFields.setLevel2FieldDesc(l2_desc);
+        levelTwoFields.setLevel1FieldId(requestJson.getInteger("L1_id"));
+        levelTwoFields.setLevel1FieldTag(requestJson.getString("L1_tag"));
+        levelTwoFields.setLevel1FieldName(requestJson.getString("L1_name"));
+        levelTwoFields.setLevel2FieldName(requestJson.getString("L2_name"));
+        levelTwoFields.setLevel2FieldDesc(requestJson.getString("L2_desc"));
 
         int res = dataTypeService.addLevelTwoFields(levelTwoFields);
 
