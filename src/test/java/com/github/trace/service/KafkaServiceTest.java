a9777a4670bb7f1601fe50f6939864d5dad2b970
chenlong <chenlong88882001@163.com>
2016-07-06 16:10:40 +0800

    编辑 工程代码 编译错误 情况

--- src/test/java/com/github/trace/service/KafkaServiceTest.java
+++ src/test/java/com/github/trace/service/KafkaServiceTest.java
@@ -1,53 +1,59 @@
 package com.github.trace.service;
 
-import org.junit.Ignore;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Map;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * KafkaServiceTest
  * Created by wzk on 16/3/23.
  */
 //@Ignore
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationContext.xml")
+@RunWith( SpringJUnit4ClassRunner.class )
+@ContextConfiguration( "classpath:applicationContext.xml" )
 @Slf4j
 public class KafkaServiceTest {
 
-  @Autowired
-  private KafkaService service;
+	private static final Logger log = LoggerFactory.getLogger( KafkaServiceTest.class );
 
-  @Test
-  public void testGetMessages() throws Exception {
-    service.getMessages("nginx.reverse", 10);
-  }
+	@Autowired
+	private KafkaService service;
 
-  @Test
-  public void testGetLastMessageTimestamp() throws Exception {
-    long ret = service.getLastMessageTimestamp("dcx.WebRequest");
+	@Test
+	public void testGetMessages() throws Exception {
+
+		service.getMessages( "nginx.reverse", 10 );
+	}
+
+	@Test
+	public void testGetLastMessageTimestamp() throws Exception {
+
+		long ret = service.getLastMessageTimestamp( "dcx.WebRequest" );
 //    long ret = service.getLastMessageTimestamp("nginx.reverse");
-    log.info("ret: {}", ret);
-  }
+		log.info( "ret: {}", ret );
+	}
 
-  @Test
-  public void testGetLastMessageTimestampWithIp() throws Exception {
-    Map<String, Long> map = service.getLastMessageTimestampWithIp("sfsfsfsf");
-    log.info("==========================");
-    log.info("result: {}", map);
-    log.info("==========================");
-  }
+	@Test
+	public void testGetLastMessageTimestampWithIp() throws Exception {
 
-  @Test
-  public void testGetOffsetSum() throws Exception {
-    long offset = service.getOffsetSum("dcx.MonitorRequest");
-    log.info("Sum of offset: {}", offset);
-  }
+		Map< String, Long > map = service.getLastMessageTimestampWithIp( "sfsfsfsf" );
+		log.info( "==========================" );
+		log.info( "result: {}", map );
+		log.info( "==========================" );
+	}
+
+	@Test
+	public void testGetOffsetSum() throws Exception {
+
+		long offset = service.getOffsetSum( "dcx.MonitorRequest" );
+		log.info( "Sum of offset: {}", offset );
+	}
 
 }
